36bf1ec713015f578283f7bdc4033e335961df93
Solomon Negusse <solomon.negusse@wri.org>
2023-01-30 11:34:38 +0300

    switch order of func args as groupby.apply expects datacube as first arg

--- zonal_datacube/analysis_functions.py
+++ zonal_datacube/analysis_functions.py
@@ -20,28 +20,28 @@ class AnalysisFunction:
     meta: Optional[Dict[str, dtype]] = None
 
 
-def _sum_func(zone, datacube):
+def _sum_func(datacube, zone):
     return Series({"sum": datacube.sum().to_pandas().sum()})
 
 
-def _count_func(zone, datacube):
+def _count_func(datacube, zone):
     # TODO how to propagate NoData value?
     return Series({"count": datacube.count().to_pandas().sum()})
 
 
-def _mean_func(zone, datacube):
+def _mean_func(datacube, zone):
     return Series({"mean": datacube.mean().to_pandas().mean()})
 
 
-def _min_func(zone, datacube):
+def _min_func(datacube, zone):
     return Series({"min": datacube.min().to_pandas().min()})
 
 
-def _max_func(zone, datacube):
+def _max_func(datacube, zone):
     return Series({"max": datacube.max().to_pandas().max()})
 
 
-def _hectare_area_func(zone, datacube):
+def _hectare_area_func(datacube, zone):
     pixel_width = datacube.latitude[0] - datacube.latitude[1]
     hectare_area_per_latitude = get_hectare_area(datacube.latitude, pixel_width)
     count_per_latitude = datacube.count(dim=["longitude"])
